aa3aca27120e76805252b8f38161f35c4c9d56a1
Linus Dufol <linusdufe@gmail.com>
2019-01-25 13:19:13 +0100

    Log in started, details improved from update

--- login.php
+++ login.php
@@ -1,16 +1,42 @@
-<div id="login">
-<h3>Login</h3>
-<form method="post" action="" name="login">
-<label>Email</label>
-<input type="text" name="usernameEmail" autocomplete="off" />
-<label>Password</label>
-<input type="password" name="password" autocomplete="off"/>
-<div class="errorMsg"><?php echo "Error"; ?></div>
-<input type="submit" class="button" name="loginSubmit" value="Login">
-</form>
-</div>
-<?php
-if (!empty($_POST['loginSubmit'])) 
+<!DOCTYPE html>
+
+<html><head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+        <title>Mantenimiento users</title>
+    </head>
+    <body> <div class="container">
+            <div class="jumbotron">
+                <h1>Editar users</h1>
+            </div>
+            <?php
+            include_once('user.php');
+            include_once('tablaclass.php');
+
+            $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
+            $surname = filter_input(INPUT_POST, "surname", FILTER_SANITIZE_STRING);
+            $birthdate = filter_input(INPUT_POST, "birthdate", FILTER_SANITIZE_STRING);
+            $address = filter_input(INPUT_POST, "address", FILTER_SANITIZE_STRING);
+            $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_STRING);
+            $pass = filter_input(INPUT_POST, "pass", FILTER_SANITIZE_STRING);
+            $id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+
+            $server = "localhost";
+            $user = "root";
+            $pass = "";
+            $db = "plugwalk";
+
+            try {
+
+                $conn = new PDO("mysql:host=$server;dbname=$db", $user, $pass);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                if (!empty($_POST['loginSubmit'])) 
 {
 $usernameEmail=$_POST['usernameEmail'];
 $password=$_POST['password'];
@@ -28,4 +54,26 @@ $errorMsgLogin="Please check login details.";
 }
 }
 }
-?>
+                // Code For LOGIN
+                        ?>
+                        <form method="POST">
+                            <div class="form-group">
+                                <label for="email">Email:</label>
+                                <input type="text" class="form-control" id="email"  name="email" value="<?= $user['email'] ?>">
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password:</label>
+                                <input type="text" class="form-control" id="password"  name="password" value="<?= $user['password'] ?>">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Enviar</button>
+                            <a href="index.php" class="btn btn-primary">Vuelve</a>
+                        </form>
+                        <?php
+            
+            } catch (PDOException $e) {
+                echo "Connection failed: " . $e->getMessage();
+            }
+            ?>
+        </div>
+    </body>
+</html>
